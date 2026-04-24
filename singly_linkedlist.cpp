9ff8c44061d03303eaf3d17f11a15a24535995fe
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-04-24 16:39:48 +0700

    menambahkan logika menu menggunakan switch case

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -130,5 +130,71 @@ int main()
         cout << endl
              << "Masukkan pilihan (1-5): ";
         cin >> ch;
-    }
+                switch (ch)
+        {
+        case '1':
+        {
+            mhs.addNode();
+        }
+        break;
+
+        case '2':
+        {
+            if(mhs.listEmpty())
+            {
+                cout << endl
+                     << "List Kosong" << endl;
+                break;
+            }
+            cout << endl;
+            cout << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+            cout << endl
+                 << "Data tidak ditemukan" << endl;
+            else
+                cout << endl;
+                cout << "Data dengan nomor mahasiswa " << nim
+                     << " berhasil dihapus " << endl;
+        }
+        break;
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "Masukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search (nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
+            }
+        }
+        break;
+        case '5':
+        {
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !." << endl;
+        }
+        break;
+        }
+    } while (ch != '5');
 }
